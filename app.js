7c9c2fb35b3b3f527ba97703fbfa980fa029f7ab
froconic <froconicdesign@gmail.com>
2019-08-14 13:54:55 -0500

    made a fix

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 'use strict';
 
-function Image(json) = {
+function Image(json) {
   //attributes
-    this.url = json.image_url;
-    this.title = json.title;
-    this.description = json.description;
-    this.keyword = json.keyword;
-    this.horns = json.horns;
-    //elements
-    this.element = $("<section class='image'></section>");
-    this.element.append(`<h2>${this.title}</h2>`);
-    this.element.append(`<img id=${this.title}-image src=${this.url}></img>`);
-    this.element.append(`<p class='description'>${this.description}</p>`);
+  this.url = json.image_url;
+  this.title = json.title;
+  this.description = json.description;
+  this.keyword = json.keyword;
+  this.horns = json.horns;
+  //elements
+  this.element = $("<section class='image'></section>");
+  this.element.append(`<h2>${this.title}</h2>`);
+  this.element.append(`<img id=${this.title}-image src=${this.url}></img>`);
+  this.element.append(`<p class='description'>${this.description}</p>`);
 
-    grabElement() {
-      return this.element;
-      console.log(this.element);
-    }
+  function grabElement() {
+    return this.element;
+    console.log(this.element);
+  }
 }
 
 $(document).ready(() => {
   const images = [];
-  const main = $('main');
+  const $main = $('main');
 
   function render() {
     let keyword = $('#keywords').val();
-    main.empty();
+    $main.empty();
     console.log('element emptied');
     for (const img of images) {
       console.log(keyword);
       if (keyword !== 'default' && img.keyword === keyword) {
-        main.append(img.grabElement());
+        $main.append(img.grabElement());
       } else {
-        main.append(img.grabElement());
+        $main.append(img.grabElement());
       }
     }
   }
+
   render();
-})
+
+});
