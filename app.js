f1cb93f2c73658bc9b92ee9dea730681d0f35d0a
froconic <froconicdesign@gmail.com>
2019-08-14 13:38:30 -0500

    created the constructor class

--- app.js
+++ app.js
@@ -1,56 +1,70 @@
-$(document).ready(function () {
-    $('#btShowImage').click(function () {
-        $.getJSON("./data/page-1.json", function (data) {
+$(document).ready(function() {
+  $('#btShowImage').click(function() {
+    $.getJSON("./data/page-1.json", function(data) {
 
-            var arrItems = [];      // The array to store JSON items.
-            $.each(data, function (index, value) {
-                arrItems.push(value);       // Push values in the array.
-            });
+      var arrItems = []; // The array to store JSON items.
+      $.each(data, function(index, value) {
+        arrItems.push(value); // Push values in the array.
+      });
 
-            // Extract values for the table header.
-            var col = [];
-            for (var i = 0; i < arrItems.length; i++) {
-                for (var key in arrItems[i]) {
-                    if (col.indexOf(key) === -1) {
-                        col.push(key);
-                    }
-                }
+      // Extract values for the table header.
+      var col = [];
+      for (var i = 0; i < arrItems.length; i++) {
+        for (var key in arrItems[i]) {
+          if (col.indexOf(key) === -1) {
+            col.push(key);
+          }
+        }
+      }
+
+      // Create a <table> element dynamically.
+      // Ref: https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
+      var table = document.createElement("table");
+
+      var tr = table.insertRow(-1); // Table row.
+
+      for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th"); // Table header.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+      }
+
+      // Add JSON data to the table as rows.
+      for (var i = 0; i < arrItems.length; i++) {
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+          var tabCell = tr.insertCell(-1);
+          if (j === 0) { // The last JSON column has image urls.
+
+            // Create an <img> element show the images.
+            var img = document.createElement('img');
+            img.src = arrItems[i].image_url; // The image source from JSON array.
+            tabCell.appendChild(img);
+          } else
+            tabCell.innerHTML = arrItems[i][col[j]];
+        }
+      }
+
+      // Finally, add the newly created <table> with data to a container.
+      var divContainer = document.getElementById("showData");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);
+
+
+
+      table.filter(function (i, v) {
+          var $t = $(this);
+          for (var d = 0; d < arrItems.length; d++) {
+            if ($t.is(":contains('" + arrItems[d] + "')")) {
+              return true;
             }
+          }
+          return false;
+        })
+        //show the rows that match.
+        .show();
 
-            // Create a <table> element dynamically.
-            // Ref: https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm 
-            var table = document.createElement("table");
-
-            var tr = table.insertRow(-1);                   // Table row.
-
-            for (var i = 0; i < col.length; i++) {
-                var th = document.createElement("th");      // Table header.
-                th.innerHTML = col[i];
-                tr.appendChild(th);
-            }
-
-            // Add JSON data to the table as rows.
-            for (var i = 0; i < arrItems.length; i++) {
-                tr = table.insertRow(-1);
-
-                for (var j = 0; j < col.length; j++) {
-                    var tabCell = tr.insertCell(-1);
-                    if (j === 0) {      // The last JSON column has image urls.
-
-                        // Create an <img> element show the images.
-                        var img = document.createElement('img');
-                        img.src = arrItems[i].image_url;   // The image source from JSON array.
-                        tabCell.appendChild(img);
-                    }
-                    else
-                        tabCell.innerHTML = arrItems[i][col[j]];
-                }
-            }
-
-            // Finally, add the newly created <table> with data to a container.
-            var divContainer = document.getElementById("showData");
-            divContainer.innerHTML = "";
-            divContainer.appendChild(table);
-        });        
     });
+  });
 });
